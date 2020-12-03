4dd47a564ea25d14a9f9a3eba14aabf430e6c0bd
unknown <crameriver@gmail.com>
2020-12-03 12:44:58 +0100

    added Google map

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -11,14 +11,12 @@ const meta = [{
 }]
 const ContactPage = () => (
   <Layouts>
-  <Row justify="center" className="bannerContact"></Row>
-  <Row justify="center" className="filterContact"></Row>
+  <Row justify="center" className="bannerContact">  <Row justify="center" className="filterContact"></Row></Row>
 <Row justify="center">
-    <Col>
-       Enim enim reprehenderit voluptate eiusmod velit ut commodo exercitation deserunt. Sunt culpa anim qui in quis voluptate ipsum cillum cillum. Ipsum culpa nulla adipisicing tempor. Laboris reprehenderit do non proident adipisicing laborum adipisicing Lorem.
-    </Col>
+<iframe className="ContactMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6308.654158601966!2d-122.39712718281599!3d37.75892824890718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f7fb63f323edb%3A0xc80c601741ad88de!2sLa%20French%20Teach!5e0!3m2!1sfr!2sfr!4v1606912095963!5m2!1sfr!2sfr" width="83%"></iframe>
+    <p className="ContactTitle">Contactez-nous</p>
 </Row>
-  <SEO title="Accueil"  meta = {meta} />
+  <SEO title="Contact"  meta = {meta} />
   <Link to="/">Go back to the homepage</Link>    
 </Layouts> 
 
